bbab5eae6cb7e6276762f0f12383bff222404b98
huangqing <huangqing@hoteamsoft.com>
2022-07-27 11:32:31 +0800

    feat: 优化打包分片

--- packages/vite-optimization/src/views/AboutView.vue
+++ packages/vite-optimization/src/views/AboutView.vue
@@ -1,8 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="wrapper">
+      <h1>This is an about page {{ obj.name }}</h1>
+      <p>
+        <button @click="click">i++</button>
+      </p>
+      <p><a-button>ant button</a-button></p>
+      <p><a-date-picker></a-date-picker></p>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { debounce } from "lodash-es";
+import { reactive } from "vue";
+
+import AButton from "ant-design-vue/es/button";
+import "ant-design-vue/lib/button/style/css";
+import ADatePicker from "ant-design-vue/lib/date-picker"; // 加载 JS
+import "ant-design-vue/lib/date-picker/style/css";
+debugger;
+// about.vue 注释测试
+console.log("about.vue console.log");
+
+let i = 1;
+const obj = reactive({ name: "关于页面", description: "描述" });
+//const cloneObj = reactive(cloneDeep(obj));
+
+const click = debounce(() => {
+  obj.name = "关于页面" + i++;
+}, 1000);
+</script>
 
 <style>
 @media (min-width: 1024px) {
--- packages/vite-optimization/vite.config.ts
+++ packages/vite-optimization/vite.config.ts
@@ -1,15 +1,80 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { visualizer } from "rollup-plugin-visualizer";
+import OptimizationPersist from "vite-plugin-optimize-persist";
+import PkgConfig from "vite-plugin-package-config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), PkgConfig(), OptimizationPersist(), visualizer()],
+
+  build: {
+    assetsDir: "assets",
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        //  打包删除console
+        drop_console: true,
+        // 打包删除 debugger
+        drop_debugger: true,
+      },
+    },
+
+    rollupOptions: {
+      output: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        chunkFileNames: (chunkInfo: any) => {
+          const facadeModuleId = chunkInfo.facadeModuleId
+            ? chunkInfo.facadeModuleId.split("/")
+            : [];
+          const fileName =
+            facadeModuleId[facadeModuleId.length - 2] || "[name]";
+          return `js/${fileName}/[name].[hash].js`;
+        },
+        // manualChunks: {
+        //   lodash: ["lodash-es"],
+        //   vue: ["vue", "pinia", "vue-router"],
+        //   antdv: ["ant-design-vue", "@ant-design/icons-vue"],
+        //   dayjs: ["dayjs"],
+        // },
+        manualChunks(id) {
+          // if (id.includes("node_modules") && id.includes("lodash-es")) {
+          //   return "lodash";
+          // } else if (id.includes("node_modules") && id.includes("vue")) {
+          //   return "vue";
+          // } else if (id.includes("node_modules")) {
+          //   return "vendor";
+          // }
+
+          if (id.includes("node_modules")) {
+            if (id.includes("lodash-es")) {
+              return "lodash";
+            } else if (id.includes("dayjs")) {
+              return "dayjs";
+            } else if (
+              id.includes("ant-design-vue") ||
+              id.includes("@ant-design")
+            ) {
+              return "ant-design-vue";
+            } else if (id.includes("vue") || id.includes("pinia")) {
+              return "vue";
+            } else {
+              return "vendor";
+            }
+          }
+        },
+        entryFileNames: "js/[name]-[hash].js",
+        assetFileNames: "[ext]/[name]-[hash].[ext]",
+      },
+    },
+  },
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
